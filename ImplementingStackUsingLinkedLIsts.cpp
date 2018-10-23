575e4d1ae1e696cd410389b65bd8bf6324f048f4
Priyesh Kumar <priyeshkkumar@gmail.com>
2018-10-23 20:17:23 +0530

    Stack~Data Structure
    Implementing Stack Using Linked Lists

---
+++ ImplementingStackUsingLinkedLIsts.cpp
@@ -0,0 +1,33 @@
+//Implementing Stack using Linked Lists....
+
+#include<iostream>
+#include<conio.h>
+#include<stdlib.h>
+
+
+typedef struct llist *node ;
+
+using namespace std ;
+
+struct llist
+{
+	int data ;
+	node next ;
+} *head ;
+
+class stack
+{
+	int top ;
+	public: void push() ;
+	        void pop() ;
+	        void display() ;
+} o1 ;
+
+void stack :: push()
+{
+	
+}
+
+int main()
+{
+}
